0ff5e228cc20bd763ba4728a392e314b35545a14
Thales Lima <medvet21@gmail.com>
2020-05-09 10:22:07 -0300

    Scripts para testar convergência

---
+++ Scripts/New_Pos_BLUPF90.R
@@ -0,0 +1,100 @@
+# ---------------------------------------------------------------------------
+# ANÁLISE DE CONVERGENCIA EM SAIDAS DO BLUPF90 
+#
+# Created at: 10-04-2015
+# Vinicius S. Junqueira and Delvan Silva
+#
+# Modificated at: 16-04-2020
+# Thales de Lima Silva 
+# ---------------------------------------------------------------------------
+
+### Carregar pacotes
+library(data.table)
+library(dplyr)
+
+### Definir diretorio de trabalho
+rm(list = ls())
+setwd('C:/Users/Thales.Thales-PC/Dropbox/UNESP GMA/Tese/Analises/Analise2/BiPEHT/')
+
+### Carregar arquivo de dados (saida do postgibbs)
+data <- fread('postgibbs_samples', 
+              col.names = c("Ef1","Ef2","Ef3","Va1", "Cov1e2", "Va2", "Ve1", "Ve2"))
+
+# ===========================================================================
+
+### Criar colunas com herdabilidades e correlacao
+data1 <- data %>% mutate(Ha1 = Va1/(Va1+Ve1),
+                         Ha2 = Va2/(Va2+Ve2),
+                         Cor1e2 = Cov1e2/(sqrt(Va1)+sqrt(Va2)))
+
+### Analise Exploratoria 
+summary(data1[,4:11])
+
+plot(data1$Va1, type = "l", xlab = "Iteracoes", ylim = c(3.5,4.5))
+
+#Salvar grafico: png("AP.png", width = 480, height = 480, units = "px", pointsize = 12,
+#    bg = "white", res = NA, family = "", restoreConsole = TRUE,
+#    type = c("windows", "cairo", "cairo-png"), antialias)
+
+
+data2 <- data1[20000:200000,] #Da um chute no valor do burn-in
+autocorr.plot(data2, lag.max= 50, auto.layout = T) #Serve para definir o thin
+
+
+### Gravar arquivo editado na pasta
+write.table(data1, 'postgibbs_samples1', sep = ' ', quote = F, row.names = F, 
+            append = F, col.names = F)
+
+# ===========================================================================
+
+# Biblioteca de utilitários para teste de convergência
+source("convergence_utils.R")
+
+# Definir parâmetros para a função Bayes Stats
+dir        = "C:/Users/Thales.Thales-PC/Dropbox/UNESP GMA/Tese/Analises/Analise2"
+data_files = c('BiPEHT')
+burnin     = 25000
+thin       = 25
+outfile    = "resultados_analises"
+
+####Colocar um loop aqui
+# Rodar a função 
+Get.Bayes.Stats(dir, data_files, burnin, thin, outfile)
+# Carregar p-valor dos parâmetros genéticos calculados
+path <- paste(dir, outfile, "P_Values.txt", sep = "/")
+p.values = fread(path, drop = 1, 
+                 col.names = c("Va1", "Cov1e2", "Va2", "Ve1", "Ve2", "Ha1", "Ha2", "Cor1e2"))
+ifelse(p.values[,] > 0.05, "convergiu!", "ainda não")
+
+
+for(i in 1:length(p.values)){
+  ifelse(p.values[i] > 0.05, "convergiu!", "ainda não")
+}
+if (p.values[,] > 0.05) {
+  "convergiu!"
+} else {"ainda não"}
+  
+#============================================================================
+
+nro_iter <- nrow(data)
+
+# Funcao para selecao de Burn-in e Thin
+conv.parameter.selection(num.iters = 20, parameter, ) {
+  set.seed(10)
+  conv <- 0
+  while(conv == 0) {
+    for burnin 0:(nro.iter/2) {  #repete até 50% do número de iterações
+      # Roda a funcao
+      GetBayesStats(dir,data_files,burnin,thin,outfile)
+      # resultado: se Geweke >= 0.05
+      if # Se não convergiu
+      thin <- thin + 1
+      burnin <- burnin + 1000
+      GetBayesStats(dir,data_files,burnin,thin,outfile)
+      else
+    }
+  }
+}  
+  
+  
+}
---
+++ Scripts/convergence_utils.R
@@ -0,0 +1,97 @@
+# Biblioteca para analise de convergencia
+
+# Obs: Caso tenha problemas com a acentuacao, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+library(coda)
+library(mcmcse)
+library(boa)
+library(miscTools)
+
+# FUNCAO PARA GERAR ESTATISTICAS A POSTERIORI 
+Get.Bayes.Stats <- function(dir, data_files, burnin, thin, outfile){
+  nf = length(data_files)
+  resul = paste(dir, '/', outfile, '/', sep='')
+  dir.create(resul, showWarnings = F)
+  direc = paste(resul, 'Estatisticas_completas.txt', sep = '')
+  fn = file(direc, 'wt')
+  grava = paste(resul, 'P_Values.txt', sep = '')
+  gw = file(grava, 'wt')
+  for(i in 1:nf){
+    data = import_file(data = data_files, i = i, dir = dir)
+    cat('Analise =', data_files[i], '\n', file = fn)
+    cat('-------------------', '\n', file = fn)
+    cat('Media a posteriori: ', colMeans(data[,4:11]), '\n', file = fn)
+    cat('Mediana posteriori: ', colMedians(data[,4:11]), '\n', file = fn)
+    cat('Moda a posteriori:  ', moda(data), '\n', file = fn)
+    cat('Desvpad posteriori: ', PSD(data), '\n', file = fn)
+    cat('Z-Geweke (p.valor): ', gewe(data), '\n', file = fn)
+    cat('Z-Geweke_(p.valor):', gewe(data), '\n', file = gw)
+    cat('Tamanho efet (ESS): ', ESS(data), '\n', file = fn)
+    cat('Intervalo de credibilidade: ', '\n', file = fn)
+    Intervalo_cred(data, fn = fn)
+  }
+  close(fn)
+}
+
+# FUNCAO PARA IMPUTAR DADOS DE FORMA RECURSIVA
+import_file <- function(data, i, dir){
+  # Import data files
+  fold = paste(dir, '/', data[i], '/postgibbs_samples1', sep='')
+  files = read.table(fold)
+  # Burnin
+  files2 = files[(burnin+1):nrow(files),]
+  # Thin samples
+  gethin = rep(1:thin, times = nrow(files2)/thin)
+  idx = gethin == thin
+  files3 = files2[idx,]
+  return(files3)
+}
+
+# FUNCAO PARA CALCULAR A "MODA"
+moda = function(data){
+  val = NULL
+  for(j in 1:(ncol(data)-3)){
+    aaa = hist(data[[3+j]], plot=F)
+    tmp = aaa$mids[aaa$counts == max(aaa$counts)]
+    val = cbind(val, tmp)
+  }
+  return(val)
+}
+
+# FUNCAO PARA CALCULAR O "TAMANHO EFETIVO DAS AMOSTRAS"
+ESS = function(data){
+  val = NULL
+  for(j in 1:(ncol(data)-3)){
+    tmp = ess(data[[3+j]])
+    val = cbind(val, tmp)
+  }
+  return(val)
+}
+
+# FUNCAO PARA CALCULAR O "DESVIO PADRAO A POSTERIORI"
+PSD = function(data){
+  val = NULL
+  for(j in 1:(ncol(data)-3)){
+    tmp = sd(data[[3+j]])
+    val = cbind(val, tmp)
+  }
+  return(val)
+}
+
+# FUNCAO PARA CALCULAR O "P-VALUE" DO TESTE GEWEKE
+gewe = function(data){
+  tmp = boa.geweke(data[,4:11], p.first = .1, p.last = .5)
+  return(tmp[,2])
+}
+
+# FUNCAO PARA CALCULAR O "INTERVALO DE CREDIBILIDADE"
+Intervalo_cred = function(data, fn){
+  val = NULL
+  for(j in 1:(ncol(data)-3)){
+    tmp1 = quantile(data[[3+j]], probs = .025)
+    tmp2 = quantile(data[[3+j]], probs = .975)
+    cat('Efeito ', j,' = ', tmp1, tmp2, '\n', file=fn)
+  }
+  return(val)
+}
